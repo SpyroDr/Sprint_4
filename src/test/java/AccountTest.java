210e0aba8bd801ada3419d5da4ccb4809c5cf76e
yurys <yury.sharypov@yandex.ru>
2022-01-09 19:40:17 +0200

    Проект 4 спринта

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,2 +1,48 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(value = Parameterized.class)
 public class AccountTest {
+    private Account account;
+    private String name;
+    private Boolean result;
+
+    public AccountTest(String name, Boolean result) {
+        this.name = name;
+        this.result = result;
+    }
+
+    @Before
+    public void setUp(){
+        account = new Account(name);
+    }
+
+    @Parameterized.Parameters(name = "{index}: для имени: {0} метод checkNameToEmboss возвращает {1} ")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                        {"Тимоти Шаламе ", false},
+                        {" Тимоти Шаламе", false},
+                        {"Тимоти  Шаламе", false},
+                        {"Тимоти Шаламе", true},
+                        {" ТимотиШаламе", false},
+                        {"ТимотиШаламе ", false},
+                        {"ТШ", false},
+                        {"Т Ш", true},
+                        {"Тимоти Шаламепаимпр", false},
+                        {"Тимоти Шаламепаимп", true},
+                        {null, false},
+                }
+        );
+    }
+    @Test
+    public void checkNameToEmbossNotCorrectNameCanNotPrint(){
+
+        assertEquals(result,account.checkNameToEmboss());
+    }
 }
